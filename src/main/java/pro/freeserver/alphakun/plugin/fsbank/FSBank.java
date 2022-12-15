80a397a5e556ad134a916a38abddae0310f235e1
kosukej <81564943+kosukej@users.noreply.github.com>
2022-12-16 00:36:26 +0900

    Initial commit

---
+++ src/main/java/pro/freeserver/alphakun/plugin/fsbank/FSBank.java
@@ -0,0 +1,17 @@
+package pro.freeserver.alphakun.plugin.fsbank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FSBank extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
